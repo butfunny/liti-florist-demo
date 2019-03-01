dbdd236169ba2602b4539d8f7974b215e6387ddb
cuongnguyen <butfunny63@gmail.com>
2019-03-01 16:34:03 +0700

    minor fix

--- src/app/common/common.js
+++ src/app/common/common.js
@@ -428,6 +428,11 @@ export function getStartAndLastDayOfWeek(curr = new Date()) {
     from.setHours(0, 0, 0, 0);
     to.setHours(23, 59, 59, 99);
 
+
+    if (from.getTime() > to.getTime()) {
+        to.setMonth(to.getMonth() + 1);
+    }
+
     return {
         from, to
     }
